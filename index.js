8b088fed4ab2865f25f2c1a850d53f4d3d354311
HuzaifaTP <91236840+HuzaifaTP@users.noreply.github.com>
2021-10-27 02:37:35 +0800

    Submission

---
+++ index.js
@@ -0,0 +1,21 @@
+class Budget101{
+
+    constructor(){
+        this.earningInput = document.querySelector(".earnings");
+        this.spendingInput = document.querySelector(".spending");
+        this.spendingEarningForm = document.querySelector(".spending_earning_form")
+        this.balanceAmount = document.querySelector(".current_Balance");
+
+        this.earningList = document.querySelector(".earning_list")
+        this.spendingList = document.querySelector(".spending_list")
+        this.spendingInput = document.querySelector(".spending_Input")
+    }
+}
+
+function listenToEvents(){
+    const spendingEarningForm = document.querySelector(".spending_earning_form");
+    const spendingList = document.querySelector(".spending_list");
+    const earningList = document.querySelector(".earning_list");
+
+    
+}
